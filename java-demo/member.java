23014003d0df6cbe1da941fbd4589cce2a0d8a59
lyz66046861 <761977365@qq.com>
2020-03-13 16:48:03 +0800

    add java-demo lib

---
+++ java-demo/member.java
@@ -0,0 +1,10 @@
+import javafx.scene.control.Menu;
+
+class member{
+    int a ;
+    public static void main(String[] args) {
+        member A= new member(); 
+        System.out.print("Default int a:"+A.a);
+    }
+}
+
