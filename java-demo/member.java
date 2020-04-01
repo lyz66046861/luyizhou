b28056d743088a92b87a0d7071ba56014b074aad
lyz66046861 <761977365@qq.com>
2020-04-01 15:43:34 +0800

    firstimechg

--- java-demo/member.java
+++ java-demo/member.java
@@ -1,16 +1,16 @@
-import java.lang.reflect.Array;
-import java.util.ArrayList;
+// import java.lang.reflect.Array;
+// import java.util.ArrayList;
 
-import javafx.scene.control.Menu;
+// import javafx.scene.control.Menu;
 
 class member{
     int a ;
     public static void main(String[] args) {
         member A= new member(); 
-        System.out.print("Default int a:"+A.a);
-        System.out.print(A.toString());
-        Class cls1 = String.class;
-        System.out.print(cls1);
+        System.out.println("Default int a:"+A.a);
+        System.out.println(A.toString());
+        // Class cls1 = String.class;
+        // System.out.print(cls1);
 
     }
 }
--- javascript-demo/closure.js
+++ javascript-demo/closure.js
@@ -1,17 +1,36 @@
-
+console.log("wqe")
 var clo = function(){
-	var x
+	var x = 3
 	return {
 		add: function (){
 			x++
 		},
 		sub: function (){
 			x--
+		},
+		show: function(){
+			console.log(x)
 		}
 	}
 }
 
 a = clo()
-a.add()
 console.log(
-	a.add())
+	a.show())
+
+function outerFn() {
+	var i = 0;
+	function innnerFn() {
+		i++;
+		console.log(i);
+	}
+	return innnerFn;
+}
+var inner1 = outerFn();
+var inner2 = outerFn();
+outerFn()();
+outerFn()();
+inner1();
+inner2();
+inner1();
+inner2();
